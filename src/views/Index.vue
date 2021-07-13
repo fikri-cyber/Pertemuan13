dab7b83b866c7d6a8dfdbc937a28fbf10b156ecc
fikri-cyber <fikrimuhammad206@gmail.com>
2021-07-13 14:56:35 +0700

    Latihan CRUD Pertemuan 13

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -12,7 +12,7 @@
                             <thead class="thead-dark">
                                 <tr>
                                 <th scoppe="col">TITLE</th>
-                                <th scoppe="col">CONTENT</th>
+                                <th scoppe="col">CONTENT</th> 
                                 <th scoppe="col">OPTIONS</th>
                                 </tr>
                             </thead>
@@ -21,37 +21,63 @@
                                     <td>{{ post.title}}</td>
                                     <td>{{ post.content}}</td>
                                     <td class="text-center">
-                                        <router-link :to="{name : 'post.edit', params:{id: post,id}}"
+                                        <router-link :to="{name : 'post.edit', params:{id: post.id}}"
                                         class="btn btn-sm btn-primary mr-1">EDIT</router-link>
-                                        <button class="btn btn-sm btn-danger mr-1">HAPUS</button>
+                                        <button
+                                        @click.prevent ="postDelete(post.id)"
+                                         class="btn btn-sm btn-danger mr-1">HAPUS</button>
 
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 <script>
 import axios from 'axios'
 import { onMounted, ref} from 'vue'
-
 export default {
     setup() {
         let posts = ref([])
         onMounted(() => {
-            axios.get('http://novaagustina.online/backendapi/public/api/post')
+            axios
+            .get('http://novaagustina.online/backendapi/public/api/post')
             .then(response => {
-                posts.value = response.data.data
-            }).catch(error => {
-                console.log(error.response.data)
+                const res = response.data;
+                const tmp = {};
+                if (res.success == true) {
+                    const data = res.data;
+                    data.forEach((item) => {
+                        tmp[item.id] = item;
+                    })
+                }
+                console.log(tmp)
+                posts.value = tmp;
             })
+            .catch((error) => {
+                console.log(error.response.data);
+            });
+        });
+    function postDelete(id) {
+        axios
+        .delete(`http://novaagustina.online/backendapi/public/api/post/${id}`)
+        .then((response) => {
+            alert(id);
+            if (response.data.success == true) {
+                delete posts.value[id];
+            }
         })
+        .catch((error)=>{
+        console.log(error.response.data);
+        });
+    } 
     return {
-        posts
+        posts,
+        postDelete,
     }
     }
 }
@@ -61,4 +87,4 @@ export default {
 body{
     background: lightgray;
 }
-</style>
+</style>  
